b0f35f81c584a9f56701aaf379214bcff8c1c38e
mesya86 <jun.calorina@gmail.com>
2026-02-28 09:45:58 +0900

    全ページのインデント調整

--- js/menus.js
+++ js/menus.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menu-toggle');
-    const menuContainer = document.getElementById('menu');
+  const menuToggle = document.getElementById('menu-toggle');
+  const menuContainer = document.getElementById('menu');
 
-    menuToggle.addEventListener('click', () => {
-        menuContainer.classList.toggle('show');
-        menuToggle.classList.toggle('active');
-    });
+  menuToggle.addEventListener('click', () => {
+      menuContainer.classList.toggle('show');
+      menuToggle.classList.toggle('active');
+  });
 });
